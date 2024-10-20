554adcb6e4d52a1b6502a8d10ea957a35c373f77
hardiksorathiya <sorathiyahardik000@gmail.com>
2024-10-20 21:35:29 +0530

    popup done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,14 @@ $button.addEventListener('click', (e) => {
   e.preventDefault();
   $wrapper.classList.toggle('toggled');
 });
+
+// ====================================
+
+$( '.sidebar-nav a' ).on( 'click', function () {
+	$( '.sidebar-nav' ).find( 'li.active' ).removeClass( 'active' );
+	$( this ).parent( 'li' ).addClass( 'active' );
+});
+
+$(function(){
+  $("#sidebar").load("inc/sidebar.html"); 
+});
